364a4688695722a4079f4f2f605373f230aadfcb
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-10-20 21:25:28 +0300

    add downscroll, antdesign

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './screens/Header/Header';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
--- src/screens/Header/Header.js
+++ src/screens/Header/Header.js
@@ -1,28 +1,21 @@
-import "./Header.css";
+import './Header.css';
+import Icon, { DownOutlined } from '@ant-design/icons';
 
 const Header = () => {
-  
-
   return (
-    <div className = "header">
-        <div className="header-gap">
-          
+    <div className="header">
+      <div className='header-main'>
+        <div className="header-gap"></div>
+        <div className="header-items items">
+          <a className="item">Главная</a>
+          <a className="item">Контакты</a>
+          <a className="item">Каталог</a>
+          <a className="item">О нас</a>
         </div>
-        <div className = "header-items items">
-          <a className = "item">
-            Главная 
-          </a>
-          <a className = "item">
-            Контакты 
-          </a>
-          <a className = "item">
-            Каталог 
-          </a>
-          <a className = "item">
-            О нас 
-          </a>
-        </div>
-        
+      </div>
+      <a className='downOutlined'>
+        <DownOutlined style={{ color: 'white', fontSize: '40px', fontWeight: '800' }}/>
+      </a>
     </div>
   );
 };
