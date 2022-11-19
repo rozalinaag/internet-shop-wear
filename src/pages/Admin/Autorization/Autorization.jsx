c45cc80f0221ef6b90764f24c972d574e9a13a9b
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2022-11-19 17:15:48 +0300

    authorization design

--- src/pages/Admin/Autorization/Autorization.jsx
+++ src/pages/Admin/Autorization/Autorization.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './Autorization.css';
 
 function Autorization() {
   return (
-    <div>
-      
+    <div >
+      <div className='auth-main'>
+        <h2>Авторизация</h2>
+        <div className='auth'>
+          <input className='auth-item auth-login' placeholder='login@mail.ru'></input>
+          <input className='auth-item auth-login' placeholder='password'></input>
+          <button className='auth-item auth-button'>Войти</button>
+        </div>
+      </div>
     </div>
   )
 }
